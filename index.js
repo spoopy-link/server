96a5b1030c0a4f43b81ca5ad195e5e564d4b3562
Gus Caplan <me@gus.host>
2017-06-26 03:50:10 -0500

    refactor

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const blacklist = fs.readFileSync('./blacklist')
 
 const index = fs.readFileSync('./index.html');
 
+const ticks = {
+  green: 'https://cdn.discordapp.com/emojis/318902154054205460.png',
+  red: 'https://cdn.discordapp.com/emojis/318902154054205460.png',
+};
+
 http.ServerResponse.prototype._end = http.ServerResponse.prototype.end;
 http.ServerResponse.prototype.end = function(...args) {
   if (args.length === 1 && typeof args[0] === 'number') {
@@ -24,11 +29,8 @@ http.ServerResponse.prototype.end = function(...args) {
   }
 };
 
-http.createServer((req, res) => {
-  if (!req.url.startsWith('/json')) return res.end(index);
-  let suspect = new URL(req.url.slice(6));
-  if (!suspect || !suspect.href) return res.end(400, { message: 'invalid url' });
-  redirects(suspect.href)
+function getFinal(url) {
+  return redirects(url)
     .then((trail) => {
       let safe = trail.length < 5;
       let fail = 0;
@@ -41,8 +43,34 @@ http.createServer((req, res) => {
           }
         }
       }
-      res.end({ trail, safe, fail });
-      console.log(`Scanned ${suspect.href} ... safe=${safe}, fail=${fail}`);
+      return { trail, safe, fail };
+    });
+}
+
+http.createServer((req, res) => {
+  const discord = req.headers['user-agent'].includes('discordbot');
+
+  if (!req.url.startsWith('/json') && !discord) return res.end(index);
+
+  let suspect = new URL(req.url.replace(/\/(json\/)?/, ''));
+  if (!suspect || !suspect.href) return res.end(400, { message: 'invalid url' });
+  getFinal(suspect.href)
+    .then((output) => {
+      if (discord) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(`<!DOCTYPE html>
+<html>
+<head>
+<meta property=og:title content="Spoopy.link (${suspect.href})">
+<meta property=og:description content="${output.safe ? 'Safe' : 'Unsafe'} link! Trail: ${output.trail.join(' -> ')}">
+<meta property=og:image content="${output.safe ? ticks.green : ticks.red}">
+<meta property=og:url content="https://spoopy.link/${suspect.href}">
+</head>
+</html>`);
+      } else {
+        res.end(output);
+      }
+      console.log(`Scanned ${suspect.href} ... safe=${output.safe}, fail=${output.fail}`);
     })
     .catch((e) => {
       console.error(e);
