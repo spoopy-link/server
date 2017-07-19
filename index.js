a277e27f4333c3d4e260e331a5577bf426222d8d
Gus Caplan <me@gus.host>
2017-07-19 00:53:40 -0500

    add strict dynamic

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ const routes = require('./routes');
 router.use((req, res, next) => {
   req.needsOG = Constants.UA_REGEX.test(req.headers['user-agent']);
   res.header('Content-Security-Policy', [
-    'default-src \'self\' s.gus.host www.google-analytics.com cdn.rawgit.com',
-    'script-src \'self\' \'nonce-inline\' s.gus.host www.google-analytics.com cdn.rawgit.com',
-    'img-src \'self\' www.google-analytics.com',
+    "default-src 'self' s.gus.host www.google-analytics.com cdn.rawgit.com",
+    "script-src 'self' 'strict-dynamic' 'nonce-inline' s.gus.host www.google-analytics.com cdn.rawgit.com",
+    "img-src 'self' www.google-analytics.com",
   ].join(';'));
   res.headers({
     'X-XSS-Protection': '1; mode=block',
